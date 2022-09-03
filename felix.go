7f206e73eff7c8cc440765360335922de42031c3
Rob8150 <robert.clark.1967@gmail.com>
2022-09-03 04:29:32 +0000

    Added some structs

--- felix.go
+++ felix.go
@@ -1,8 +1,77 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+type Address struct {
+	street  string
+	city    string
+	zip     string
+	state   string
+	country string
+}
+
+type Suppliers struct {
+	Sup []Supplier
+}
+
+type Supplier struct {
+	id             string
+	name           string
+	PaymentAddress Address
+	Merchendize    []Product
+}
+
+type Customers struct {
+	cust []Customer
+}
+
+type Customer struct {
+	id        string
+	FirstName string
+	LastName  string
+	Shipping  Address
+	Billing   Address
+	Cart      []Product
+}
+
+type Product struct {
+	pks      string
+	name     string
+	desc     string
+	category string
+	retail   float32
+	sale     float32
+	discount float32
+	qty      int
+}
+
+type inventry struct {
+	id    string
+	stock []Product
+}
+
+type purchaseOrder struct {
+	id       string
+	datetime string
+	status   string
+	items    []Product
+}
 
 func main() {
-	fmt.Println("felix retail loop")
-	fmt.Println("Testing Remote push")
+	var (
+		tim int = 1000
+	)
+
+	fmt.Println("Retail Loop")
+	for i := 0; i < 10; i++ {
+		sleeper(tim)
+		fmt.Println(i)
+	}
+}
+
+func sleeper(tim int) {
+	time.Sleep(time.Duration((tim)) * time.Millisecond)
 }
